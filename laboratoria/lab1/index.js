55b124f01328bb6fa53b6dd00ff19691494b1017
kacprow21324 <kawoszczylo.work@gmail.com>
2025-10-06 11:43:03 +0200

    ZADANIE 4
     NPM

--- laboratoria/lab1/index.js
+++ laboratoria/lab1/index.js
@@ -1,24 +1,7 @@
-const http = require('http');
-const path = require("path");
-const util = require("util");
-const v8 = require("v8");
+import uniqueString from 'unique-string';
 
-const hostname = '127.0.0.1';
-const port = 8888;
+const fs = require("fs");
+const data = fs.readFileSync('package.json');
 
-const server = http.createServer((request, response) => {
-
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello, from node.js serwer !\n');
-    
-});
-
-server.listen(port, hostname, () => {
-
-    util.log(v8.getHeapStatistics());
-    console.log(path.basename(__filename));
-    util.log(path.join(__dirname, 'uploads','images'));
-    console.log(`Server running at http://${hostname}:${port}/`);
-
-});
+console.log(uniqueString());
+console.log("\ngenerowanie unikatowego losowego napisu.");
